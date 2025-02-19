a37e6d5630512be1a94184348fc84904768a0c5e
Connor Payne <connor@stakehauslabs.com>
2025-02-19 20:58:03 +0100

    add dutch auction tests

--- dutch-auction/test/DutchAuction.t.sol
+++ dutch-auction/test/DutchAuction.t.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {DutchAuction} from "../src/DutchAuction.sol";
-import {EnglishAuction, IERC721} from "../src/EnglishAuction.sol";
+import {DutchAuction, IERC721} from "../src/DutchAuction.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TestNFT is ERC721 {
@@ -18,24 +17,32 @@ contract DutchAuctionTest is Test {
     IERC721 public nft;
     address public buyer;
 
-
     receive() external payable {}
 
     function setUp() public {
-        dutchAuction = new DutchAuction();
         vm.deal(address(this), 100 ether);
         TestNFT testNft = new TestNFT();
         nft = IERC721(address(testNft));
         nft.mint(address(this), 1);
-        dutchAuction = new DutchAuction(address(nft), 1, 1 ether);
+        dutchAuction = new DutchAuction(100 ether, 0.0001 ether, address(nft), 1);
         buyer = address(0x1);
         vm.deal(buyer, 100 ether);
     }
 
     function test_buy() public {
+        assertEq(nft.ownerOf(1), address(this));
+        nft.approve(address(dutchAuction), 1);
         vm.prank(buyer);
-        dutchAuction.buy{value: 1 ether}();
+        dutchAuction.buy{value: 100 ether}();
+        vm.stopPrank();
         assertEq(nft.ownerOf(1), buyer);
+    }
+
+    function test_auction_expired() public {
+        vm.warp(block.timestamp + 8 days);
+        vm.prank(buyer);
+        vm.expectRevert("auction expired");
+        dutchAuction.buy{value: 100 ether}();
         vm.stopPrank();
     }
 }
